4799bf2925af62300605df485be455f56763dc67
julienemo <juliette.nada@gmail.com>
2020-05-12 12:50:47 +0200

    [project] updated translations

--- multilingualportfolio/src/Translations/En/index.js
+++ multilingualportfolio/src/Translations/En/index.js
@@ -2,12 +2,14 @@ import contact from './contact.json';
 import presentation from './presentation.json';
 import title from './title.json';
 import work from './work.json';
+import links from './links.json';
 
 const EN = {
   ...contact,
   ...presentation,
   ...title,
   ...work,
+  ...links,
 };
 
 export default EN;
--- multilingualportfolio/src/Translations/Fr/index.js
+++ multilingualportfolio/src/Translations/Fr/index.js
@@ -2,12 +2,14 @@ import contact from './contact.json';
 import presentation from './presentation.json';
 import title from './title.json';
 import work from './work.json';
+import links from './links.json';
 
 const FR = {
   ...contact,
   ...presentation,
   ...title,
   ...work,
+  ...links,
 };
 
 export default FR;
