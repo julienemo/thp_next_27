d1fea6e47b7befebf246d3c3dac63b50353ce5d2
julienemo <juliette.nada@gmail.com>
2020-05-12 11:28:42 +0200

    [project] added translation files

---
+++ multilingualportfolio/src/Translations/En/index.js
@@ -0,0 +1,13 @@
+import contact from './contact.json';
+import presentation from './presentation.json';
+import title from './title.json';
+import work from './work.json';
+
+const EN = {
+  ...contact,
+  ...presentation,
+  ...title,
+  ...work,
+};
+
+export default EN;
---
+++ multilingualportfolio/src/Translations/Fr/index.js
@@ -0,0 +1,13 @@
+import contact from './contact.json';
+import presentation from './presentation.json';
+import title from './title.json';
+import work from './work.json';
+
+const FR = {
+  ...contact,
+  ...presentation,
+  ...title,
+  ...work,
+};
+
+export default FR;
